11a1e9b123ed547f356ce7c48c15c1569fe62ee1
unknown <D:\The Bat!>
2016-01-28 23:25:13 +0200

    some not working must be change

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,19 @@
 public class Calculate {
 	public static void main(String[] arg){
-	System.out.println("Calculate is running");
-	
+		System.out.println("Calculate is running");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[2]);
+		String operation = arg[1];
+		int resalts = 0;
+		System.out.println("operation is " + operation);
+		if(operation == String.valueOf("+")) { resalts = first + second;
+		System.out.println(first + " " + operation + " " + second + " = "  + resalts);}
+		if(operation == String.valueOf("-")) { resalts = first - second;
+		System.out.println(first + " " + operation + " " + second + " = "  + resalts);}
+		if(operation == String.valueOf("*")) { resalts = first * second;
+		System.out.println(first + " " + operation + " " + second + " = "  + resalts);}
+		if(operation == String.valueOf("/")) { resalts = first / second;
+		System.out.println(first + " " + operation + " " + second + " = "  + resalts);}
+		System.out.println("Nothing work ((");
 	}
 }
